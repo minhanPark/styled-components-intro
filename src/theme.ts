34ccaec53d7e3d4cea4847a0d2c200d4125f0e6b
Running water <alsgks1218@naver.com>
2022-02-09 13:20:44 +0900

    add Route

--- src/theme.ts
+++ src/theme.ts
@@ -1,11 +1,7 @@
 import { DefaultTheme } from "styled-components";
 
-export const lightTheme: DefaultTheme = {
-  textColor: "black",
-  bgColor: "white",
-};
-
-export const darkTheme: DefaultTheme = {
-  textColor: "white",
-  bgColor: "black",
+export const theme: DefaultTheme = {
+  bgColor: "#2f3640",
+  textColor: "#f5f6fa",
+  accentColor: "#4cd137",
 };
